36ea5acf59c19c675a35fb666e7f5b0930bd2ce5
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-05-26 17:50:38 -0300

    Base project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_ui/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,11 +12,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Travel UI',
       theme: ThemeData(
-        //primarySwatch: Color(0xff3EBACE),
-        //accentColor: Color(0xffD8ECF1),
-        //scaffoldBackgroundColor: Color(0xffF3F5F7)
+        primaryColor: Color(0xff3EBACE),
+        accentColor: Color(0xffD8ECF1),
+        scaffoldBackgroundColor: Color(0xffF3F5F7)
       ),
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
