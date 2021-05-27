0d5f38aaef3bccbebd2e2e85786d0ed690791f3d
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-05-26 21:40:30 -0300

    Destination carousel complete. Images url adjusted

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel_ui/widgets/destination_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -17,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget buildIcon(int index) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         setState(() {
           _selected = index;
         });
@@ -26,12 +27,16 @@ class _HomeScreenState extends State<HomeScreen> {
         width: 60,
         height: 60,
         decoration: BoxDecoration(
-            color: _selected == index? Theme.of(context).accentColor: Color(0xffE7EBEE),
+            color: _selected == index
+                ? Theme.of(context).accentColor
+                : Color(0xffE7EBEE),
             borderRadius: BorderRadius.circular(30)),
         child: Icon(
           icons[index],
           size: 25,
-          color: _selected == index? Theme.of(context).primaryColor: Color(0xffb4C1C4),
+          color: _selected == index
+              ? Theme.of(context).primaryColor
+              : Color(0xffb4C1C4),
         ),
       ),
     );
@@ -64,7 +69,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   )
                   .toList(),
             ),
-
+            SizedBox(
+              height: 20,
+            ),
+            DestinationCarousel()
           ],
         ),
       ),
---
+++ lib/widgets/destination_carousel.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel_ui/models/destination_model.dart';
+
+class DestinationCarousel extends StatelessWidget {
+  const DestinationCarousel({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Top Destinations",
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5),
+              ),
+              GestureDetector(
+                child: Text(
+                  "See All",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1),
+                ),
+                onTap: () {
+                  print("See All");
+                },
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 300,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: destinations.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 210,
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Positioned(
+                      bottom: 15,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        width: 200,
+                        height: 120,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "${destinations[index].activities.length} activities",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Text(
+                                "${destinations[index].description}",
+                                style: TextStyle(color: Colors.grey),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black26,
+                              offset: Offset(0, 2),
+                              blurRadius: 6)
+                        ],
+                      ),
+                      child: Stack(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image(
+                                fit: BoxFit.cover,
+                                width: 180,
+                                height: 180,
+                                image:
+                                    AssetImage(destinations[index].imageUrl)),
+                          ),
+                          Positioned(
+                            left: 10,
+                            bottom: 10,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  destinations[index].city,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 25),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(FontAwesomeIcons.locationArrow,
+                                        size: 10, color: Colors.white),
+                                    Text(
+                                      destinations[index].country,
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
