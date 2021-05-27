c491bffd1b0e2c90aaf9b7a0bf39c05a0f5f2a11
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-05-27 20:25:22 -0300

    Create:  * bottomNavigationBar  * ListView.builder for hotels

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travel_ui/widgets/destination_carousel.dart';
+import 'package:travel_ui/widgets/hotel_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selected = 0;
+  int _currentTab = 0;
   List<IconData> icons = [
     FontAwesomeIcons.plane,
     FontAwesomeIcons.bed,
@@ -72,7 +74,42 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 20,
             ),
-            DestinationCarousel()
+            DestinationCarousel(),
+            SizedBox(
+              height: 20,
+            ),
+            HotelCarousel()
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (int value) {
+            setState(() {
+              _currentTab = value;
+            });
+          },
+          currentIndex: _currentTab,
+          selectedFontSize: 0,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                size: 30,
+              ),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.local_pizza,
+                  size: 30,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: CircleAvatar(
+                  radius: 15,
+                  backgroundImage: NetworkImage(
+                      "https://media-exp1.licdn.com/dms/image/C4D03AQH4YMerD0HfyQ/profile-displayphoto-shrink_800_800/0/1619046395397?e=1627516800&v=beta&t=LLGv9Ki_ag5dm4DFXZbgxf5vZRtybPhaU_655SVVO00"),
+                ),
+                label: ""),
           ],
         ),
       ),
---
+++ lib/widgets/hotel_carousel.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:travel_ui/models/hotel_model.dart';
+
+class HotelCarousel extends StatelessWidget {
+  const HotelCarousel({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Exclusive Hotels",
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5),
+              ),
+              GestureDetector(
+                child: Text(
+                  "See All",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1),
+                ),
+                onTap: () {
+                  print("See All");
+                },
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 300,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: hotels.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 240,
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Positioned(
+                      bottom: 15,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        width: 240,
+                        height: 120,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                hotels[index].name,
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(height: 2),
+                              Text(
+                                hotels[index].address,
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              SizedBox(height: 2),
+                              Text(
+                                "\$${hotels[index].price} /night",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black26,
+                              offset: Offset(0, 2),
+                              blurRadius: 6)
+                        ],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image(
+                            fit: BoxFit.cover,
+                            width: 220,
+                            height: 180,
+                            image: AssetImage(hotels[index].imageUrl)),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
